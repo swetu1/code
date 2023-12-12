454b57909780ec81de180f4fc64722f754e24ac5
swetu1 <149421367+swetu1@users.noreply.github.com>
2023-12-12 16:23:16 +0530

    Create Count the number of possible triangles using two pointer approach.cpp

---
+++ Count the number of possible triangles using two pointer approach.cpp
@@ -0,0 +1,45 @@
+// C++ implementation of the above approach
+#include <bits/stdc++.h>
+using namespace std;
+
+void CountTriangles(vector<int> A)
+{
+
+	int n = A.size();
+
+	sort(A.begin(), A.end());
+
+	int count = 0;
+
+	for (int i = n - 1; i >= 1; i--) {
+		int l = 0, r = i - 1;
+		while (l < r) {
+			if (A[l] + A[r] > A[i]) {
+
+				// If it is possible with a[l], a[r]
+				// and a[i] then it is also possible
+				// with a[l+1]..a[r-1], a[r] and a[i]
+				count += r - l;
+
+				// checking for more possible solutions
+				r--;
+			}
+			else
+
+				// if not possible check for
+				// higher values of arr[l]
+				l++;
+		}
+	}
+	cout << "No of possible solutions: " << count;
+}
+
+// Driver code
+int main()
+{
+
+	vector<int> A = { 10, 21, 22, 100, 101, 200, 300 };
+
+	// Function call
+	CountTriangles(A);
+}
